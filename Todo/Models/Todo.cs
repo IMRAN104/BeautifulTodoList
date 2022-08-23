d4c117f4e03e0f65f91e046bbbb635e76e8a3e2b
Imran Emon <iemon@medavante.com>
2022-08-23 10:31:18 +0600

    Added Form Template

---
+++ Todo/Models/Todo.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models;
+
+public class Todo
+{
+    // [Key]
+    // [Required]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    public int MyProperty { get; set; }
+}
+enum Priority
+{
+    Low = 1,
+    Medium = 3,
+    High = 5
+}
